a2b0ecc977c6e407d54846210cd447ca757fd0de
GustavoRFG <gustavorfg@hotmail.com>
2025-04-07 03:59:41 -0300

    Projeto EducBot

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,8 @@ export default function Home() {
     setResponse(null);
 
     try {
-      const res = await fetch("http://localhost:5001/rag", {
+      // const res = await fetch("http://localhost:5001/rag", {
+        const res = await fetch("https://bdb8-2804-1b2-11c0-5e9d-a653-33c6-db20-97cc.ngrok-free.app/rag", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }),
